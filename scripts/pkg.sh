69aa78609fdc766ef903e335fbfd62cc684d781a
Ribbon <55782009+hardBSDk@users.noreply.github.com>
2023-01-17 19:28:29 -0300

    Update pkg.sh

--- scripts/pkg.sh
+++ scripts/pkg.sh
@@ -1,3 +1,3 @@
 su
 pkg update
-pkg install -y doas nano htop xfce mesa-drivers firefox-esr vlc git cmake meson
+pkg install -y doas nano htop xfce mesa-drivers firefox-esr vlc git cmake meson drm-kmod wget
