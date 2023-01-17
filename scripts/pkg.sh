13cf456cc4a8cd115eb219c3e2a25cbd8fee9ef0
Ribbon <55782009+hardBSDk@users.noreply.github.com>
2023-01-17 20:14:24 -0300

    Fixed become root.

--- scripts/pkg.sh
+++ scripts/pkg.sh
@@ -1,3 +1,1 @@
-su
-pkg update
-pkg install -y doas nano htop xfce mesa-drivers firefox-esr vlc git cmake meson drm-kmod wget
+su -m root -c "pkg update ; pkg install -y doas nano htop xfce mesa-drivers firefox-esr vlc git cmake meson drm-kmod wget ; exit"
